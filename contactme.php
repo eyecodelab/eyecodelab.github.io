148954d0b2bb01b6bde420a8a01a1598d4ba17b3
Doãn Đình Hảo <doandinhhao2004@gmail.com>
2025-06-10 15:30:57 +0700

    fix: switch langue, font page; update : link gg form in career page

--- contactme.php
+++ contactme.php
@@ -7,12 +7,7 @@
     $message = $_POST["message"];
 
     $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
-
-<<<<<<< HEAD
-    $status = mailfunction("eyecode@eyecodetech.com", "Company", $body); //reciever
-=======
     $status = mailfunction("eyecodelab@gmail.com", "Company", $body); //reciever
->>>>>>> hao-dev
     if($status)
         echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
     else
